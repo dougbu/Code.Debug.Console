f5bd73bd3f143598b16fd0d37b83107e97165999
Doug Bunting <dougbu@microsoft.com>
2016-05-08 22:03:45 -0700

    Initial commit of initial files, initially

---
+++ src/Code.Debug.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Code.Debug.Console
+{
+    public class Program
+    {
+        private static readonly string Joe = "hello";
+        private readonly List<int> _list = new List<int>
+        {
+            1,
+            2,
+            3,
+        };
+        protected Complex _complex = new Complex();
+
+        public static void Main(string[] args)
+        {
+            new Program().Public(42);
+        }
+
+        public string Hello { get; } = "hello";
+
+        public void Public(int topLevel)
+        {
+            var joe = Joe;
+            Internal(joe);
+        }
+
+        internal void Internal(string nextLevel)
+        {
+            Protected(nextLevel);
+        }
+
+        protected void Protected(string nextNextLevel)
+        {
+            Private(23);
+        }
+
+        private void Private(int bottomLevel)
+        {
+            var complex = new Complex();
+        }
+
+        protected class Complex
+        {
+            public string Hello { get; } = "hello";
+
+            public Complex LessComplex { get; }
+        }
+    }
+}
---
+++ src/Code.Debug.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Code.Debug.Console")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e0d31f4-27c5-42c8-85d8-c22178f59876")]
